e810047993fe87888a4f43e1f569ccaa0a5bbbf3
nahom09 <kalkidandemelash2127@gmail.com>
2023-03-23 10:38:11 -0700

    manatory tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+es (11 sloc)  161 Bytes
+
+#include "main.h"
+
+/**
+*mul - multiplies two numbers
+*@a: first number
+*@b: second number
+*Return: returns result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
